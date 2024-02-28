f7294816bcaa31fcf21eeae70826fdd48a33e898
R1013-T <nosuke1013@icloud.com>
2024-02-28 21:06:57 +0900

    component: Update header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import AuthLink from "@/components/common/auth-link";
-import Logo from "@/components/common/logo";
+import AuthLink from "@/components/common/buttons/auth-link";
 import TopHeader from "@/components/header";
 
 export default function Home() {
   return (
     <main className="flex flex-col h-screen">
-      <TopHeader />
+      <TopHeader is_link={true} />
       <article className="flex justify-center items-center p-3 h-full" >
         <AuthLink />
       </article>
