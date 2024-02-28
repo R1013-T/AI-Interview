514d346cadd99e7f5612fa4f2db5590daa7404ec
R1013-T <nosuke1013@icloud.com>
2024-02-28 18:07:24 +0900

    setup storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,32 @@
+import type { StorybookConfig } from '@storybook/nextjs'
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  webpackFinal(baseConfig) {
+    baseConfig.resolve!.alias = {
+      ...baseConfig.resolve!.alias,
+    }
+    baseConfig.resolve!.plugins = [
+      ...(baseConfig.resolve!.plugins || []),
+      new TsconfigPathsPlugin(),
+    ]
+    return baseConfig
+  },
+  staticDirs: ['../public'],
+}
+export default config
