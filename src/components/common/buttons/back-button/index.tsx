ed7445e09a90584c55b3940686b1d0120cc0a6da
R1013-T <nosuke1013@icloud.com>
2024-02-28 19:05:15 +0900

    component: Update back button

--- src/components/common/buttons/back-button/index.tsx
+++ src/components/common/buttons/back-button/index.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link'
 import { HiChevronLeft } from 'react-icons/hi2'
 
 export default function BackButton({ href }: { href: string }) {
   return (
-    <div className="flex gap-1 items-center cursor-pointer">
+    <Link href={href} className="flex gap-1 items-center cursor-pointer">
       <HiChevronLeft className="w-5 h-5" />
-      <p className='pt-0.5' >もどる</p>
-    </div>
+      <p className="pt-0.5">もどる</p>
+    </Link>
   )
 }
