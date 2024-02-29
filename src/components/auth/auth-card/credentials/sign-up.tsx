a548a84f973dbc39d3e7823d90801d1f37004fa5
R1013-T <nosuke1013@icloud.com>
2024-02-29 16:40:27 +0900

    page: Create sign up page

---
+++ src/components/auth/auth-card/credentials/sign-up.tsx
@@ -0,0 +1,138 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useState, useTransition } from 'react'
+import { useForm } from 'react-hook-form'
+import type * as z from 'zod'
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { signUpSchema } from '@/lib/schemas/auth'
+
+import { FormError } from '../../form-error'
+
+import { useSearchParams } from 'next/navigation'
+
+export default function SignUp() {
+  const searchParams = useSearchParams()
+  const email = searchParams.get('email')
+
+  const [error, setError] = useState<string | undefined>('')
+  const [isPending, startTransition] = useTransition()
+
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: email!,
+      password: '',
+      confirm_password: '',
+      name: '',
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof signUpSchema>) => {
+    setError('')
+
+    startTransition(async () => {
+      // const result = await signIn(values)
+      // if (!result.isSuccess) {
+      //   setError(result.error.message)
+      //   return
+      // }
+    })
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3.5">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-semibold">メールアドレス</FormLabel>
+              <FormControl>
+                <Input
+                  className="placeholder:text-foreground/10"
+                  placeholder="you@example.com"
+                  {...field}
+                  disabled={true}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-semibold">パスワード</FormLabel>
+              <FormControl>
+                <Input
+                  className="placeholder:text-foreground/10"
+                  type="password"
+                  placeholder="password"
+                  {...field}
+                  autoFocus
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirm_password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-semibold">パスワード（確認）</FormLabel>
+              <FormControl>
+                <Input
+                  className="placeholder:text-foreground/10"
+                  type="password"
+                  placeholder="password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-semibold">名前</FormLabel>
+              <FormControl>
+                <Input
+                  className="placeholder:text-foreground/10"
+                  placeholder="名前"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormError message={error} />
+        <button
+          className="bg-primary w-full py-2 rounded-md border border-secondary text-foreground"
+          type="submit"
+          disabled={isPending}
+        >
+          サインアップ
+        </button>
+      </form>
+    </Form>
+  )
+}
