23e20f5603966d0aff3e69e84dc3e573bd88a3a4
R1013-T <nosuke1013@icloud.com>
2024-03-08 20:13:17 +0900

    Add @azure/openai and "ai" dependencies, and create a new chat route

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,22 @@
+import { AzureKeyCredential, OpenAIClient } from '@azure/openai'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+
+const client = new OpenAIClient(
+  process.env.AZURE_OPENAI_ENDPOINT!,
+  new AzureKeyCredential(process.env.AZURE_OPENAI_API_KEY!),
+)
+
+export const runtime = 'edge'
+
+export async function POST(request: Request) {
+  const { messages } = await request.json()
+
+  const response = await client.streamChatCompletions(
+    process.env.AZURE_OPENAI_DEPLOYMENT_NAME_35!,
+    messages,
+  )
+
+  const stream = OpenAIStream(response)
+
+  return new StreamingTextResponse(stream)
+}
